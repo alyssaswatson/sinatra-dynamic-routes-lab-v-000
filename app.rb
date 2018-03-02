20abc4df23982bfa37ae74313277480849c3c9c1
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-02 15:08:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,6 +14,6 @@ class App < Sinatra::Base
 
   get "/say/:number/:phrase" do
     phrase = params[:phrase]
-    params[:number].to_i.times{puts s}
+    params[:number].to_i.times{puts phrase}
   end
 end
